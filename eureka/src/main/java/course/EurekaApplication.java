2e8e009737bb7c83dc57d05f4aa70d57885a8a96
wei-zi-hao <853934846@qq.com>
2020-05-09 16:44:40 +0800

    1.eureka服务启动成功 2.添加日志文件

--- eureka/src/main/java/course/EurekaApplication.java
+++ eureka/src/main/java/course/EurekaApplication.java
@@ -1,13 +1,23 @@
 package course;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.ConfigurableEnvironment;
 
+/**
+ * @author eric
+ */
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
-
+	private static final Logger log = LoggerFactory.getLogger(EurekaApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
+		ConfigurableEnvironment evn = SpringApplication.run(EurekaApplication.class, args).getEnvironment();
+		log.info("启动成功！");
+		log.info("127.0.0.1:{}",evn.getProperty("server.port"));
 	}
 
 }
