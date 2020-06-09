01c09ffd40a3423dc80c14ace371a847a982e6c0
wei-zi-hao <853934846@qq.com>
2020-06-09 09:52:37 +0800

    1，后台增加枚举类， 2，前端增加全局过滤器，小节模块的收费字段使用下拉选项

--- admin/src/main.js
+++ admin/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Axios from "axios";
+import filter from "./filter/filter";
 
 Vue.config.productionTip = false
 Vue.prototype.$ajax = Axios
@@ -12,5 +13,8 @@ new Vue({
 }).$mount('#app')
 
 
+// 全局过滤器
+Object.keys(filter).forEach(key => {
+  Vue.filter(key, filter[key])
+});
 
-console.log("环境",process.env.NODE_ENV)
